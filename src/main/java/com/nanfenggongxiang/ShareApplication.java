c6f36844122bc495754c970006868eb2a42ed867
skyisbule <heqiut@qq.com>
2018-04-03 12:52:56 +0800

    swagger

--- src/main/java/com/nanfenggongxiang/ShareApplication.java
+++ src/main/java/com/nanfenggongxiang/ShareApplication.java
@@ -1,9 +1,11 @@
 package com.nanfenggongxiang;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableSwagger2Doc
 @SpringBootApplication
 @MapperScan("com.nanfenggongxiang")
 public class ShareApplication {
